0157c3bb377656655d16a6427542c69ec935088f
Alexandr <aleks.petrischew@gmail.com>
2022-04-10 15:14:36 +0600

    добавил кнопку профиль

--- main.py
+++ main.py
@@ -25,14 +25,10 @@ class MyApp(App):
     def build(self):
         mapview = MapView(zoom=12, lat=54.985303, lon=73.3911524, pos=(500,400))
         box = BoxLayout()
-        first_button = Button(text="Начало работы(Кнопка)", size_hint=(.5, .25), pos_hint={'x':.400, 'top':.300})
-        first_button.bind(on_press=self.first_button_pressed)
-        box.add_widget(self.label)
-        box.add_widget(first_button)
-
-        people_list = ScrollView(size_hint=(1, None), size=(150, 480))
-        Oleg = Label(text="Пчеловод Олег", size_hint=(.10, .100), pos=(0,0))
-        people_list.add_widget(Oleg)
+        MyApp().run()
+        box.add_widget(mapview)
+        profile = Button(size_hint=(0.2, 0.2), text=("Профиль"))
+        mapview.add_widget(profile)
 
         return mapview,box
 
@@ -41,3 +37,4 @@ class MyApp(App):
 
 if __name__ == "__main__":
     MyApp().run()
+
